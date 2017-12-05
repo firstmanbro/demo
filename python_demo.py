c740a7c2a2d46ac08c58b468bf19b5bfbac7d638
first <moonlight321000@gmail.com>
2017-12-05 21:12:18 +0530

    new python for loop demo added

---
+++ python_demo.py
@@ -0,0 +1,10 @@
+#!/bin/python
+
+
+a = []
+
+print 'list "a" before appending integers data---->{0}'.format(a)
+for i in range(1, 100):
+    a.append(i)
+
+print 'list "a" after appending data ----->{0}'.format(a)
